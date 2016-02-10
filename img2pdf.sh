2141cdcd019b3ba145e97ea363b0980516506169
Eric Stroczynski <ericstroczynski@gmail.com>
2016-02-09 16:07:55 -0800

    combines multiple pdf's from the same question into one pdf file

--- img2pdf.sh
+++ img2pdf.sh
@@ -10,11 +10,23 @@ if [[ "$aNum" -gt 0 && "$aNum" -lt 5 ]]
    .JPG|.jpeg|.PNG|.png|.TIFF|.tiff|.GIF|.gif)
    echo "Aww yiss!"
 
+	 # reads in all files of name type q1a."$fExt" and outputs pdf's
    while read -r name; do 
      fileName=$(basename "$name")
-     convert "$name" "$(dirname "$name")assign$aNum${fileName/$fExt/pdf}"
-     printf "Converting %s to %s\n" "$name" "$(dirname "$name")assign$aNum${fileName/$fExt/pdf}"
-   done < <(find . -maxdepth 1 -type f -regextype posix-extended -regex '^./q[0-9]{1,2}[a-z]?'"$fExt"'$')
+     convert "$name" "assign$aNum${fileName/$fExt/.pdf}"
+     printf "Converting %s to %s\n" "$name" "assign$aNum${fileName/$fExt/.pdf}"
+   done < <(find . -maxdepth 1 -type f -regextype posix-extended -regex '^./q[1-9]{1,2}[a-z]?'"$fExt"'$')
+	 
+	 # makes arbitrary error output file in case multiple pdf files don't exist for a particular question
+	 touch blehhh123.txt
+	 
+	 # combines questions with multiple pdf's associated into one pdf file
+	 for f in {1..9}; do 
+		 pdfunite assign"$aNum"q$f* assign"$aNum"q$f.pdf &> ./blehhh123.txt;
+	 done
+	 
+	 # removes arbitrary output file
+	 rm ./blehhh123.txt
    ;;
    *)
     echo "Not an acceptable file extension, retry."

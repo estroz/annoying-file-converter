87d7ba15ca93765b921740cb01c1d0fc9a4fd062
Eric Stroczynski <ericstroczynski@gmail.com>
2016-10-16 16:38:51 -0700

    Added error checking in union loop, changed some random syntax

--- img2pdf.sh
+++ img2pdf.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 
 echo -n "Which assignment is this? Enter assignment number: "
-read aNum
-if [[ "$aNum" -gt 0 && "$aNum" -lt 5 ]]
+read assign_num
+if [[ "$assign_num" -gt 0 && "$assign_num" -lt 5 ]]
  then
-  echo -n "Which file extension are you converting (with '.' prefix): "
-  read fExt
-  case "$fExt" in
-   .JPG|.jpeg|.PNG|.png|.TIFF|.tiff|.GIF|.gif)
-   echo "Aww yiss!"
-
-	 # reads in all files of name type q1a."$fExt" and outputs pdf's
-   while read -r name; do 
-     fileName=$(basename "$name")
-     convert "$name" "assign$aNum${fileName/$fExt/.pdf}"
-     printf "Converting %s to %s\n" "$name" "assign$aNum${fileName/$fExt/.pdf}"
-   done < <(find . -maxdepth 1 -type f -regextype posix-extended -regex '^./q[1-9]{1,2}[a-z]?'"$fExt"'$')
-	 
-	 # makes arbitrary error output file in case multiple pdf files don't exist for a particular question
-	 touch blehhh123.txt
-	 
-	 # combines questions with multiple pdf's associated into one pdf file
-	 for f in {1..9}; do 
-		 pdfunite assign"$aNum"q$f* assign"$aNum"q$f.pdf &> ./blehhh123.txt;
+  echo -n "Which file extension are you converting (one of .JPG|.jp[e]g|.PNG|.png|.TIFF|.tiff|.GIF|.gif): "
+  read file_ext
+  case "$file_ext" in .JPG|.jpeg|.jpg|.PNG|.png|.TIFF|.tiff|.GIF|.gif)
+	 # reads in all files of name type q1a."$file_ext" and outputs pdf's
+   while read -r image_name; do
+     file_name=$(basename "$image_name")
+     dest_file="assign$assign_num${file_name/$file_ext/.pdf}"
+     convert "$image_name" "$dest_file"
+     printf "Converting %s to %s\n" "${image_name#./}" "$dest_file"
+   done < <(find . -maxdepth 1 -type f -regextype posix-extended -regex '^./q[1-9]{1,2}[a-z]?'${file_ext}'$')
+	 # combines questions with multiple associated pdf's into one pdf
+	 for f in {1..9}; do
+     assign_list=$(find . -type f -name 'assign'${assign_num}'q'${f}'*')
+     [[ -z "$assign_list" ]] && break;
+		 pdfunite $assign_list assign${assign_num}q${f}.pdf
 	 done
-	 
-	 # removes arbitrary output file
-	 rm ./blehhh123.txt
    ;;
    *)
     echo "Not an acceptable file extension, retry."
+    exit
    ;;
    esac
  else
-  echo "Incorrect entry, retry."
+  echo "Incorrect assignment number, retry."
 fi
-
-
